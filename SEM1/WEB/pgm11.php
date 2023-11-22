132397ad07128c203d040c013beeb10abe9ec33e
manasis5 <150990515+manasis5@users.noreply.github.com>
2023-11-22 05:09:30 +0000

    Add files via upload

---
+++ SEM1/WEB/pgm11.php
@@ -0,0 +1,18 @@
+<?php
+$students = array(
+    "John",
+    "Alice",
+    "Bob",
+    "Charlie",
+    "Eve"
+);
+echo "Original array:\n";
+print_r($students);
+asort($students);
+echo "\nSorted array in ascending order:\n";
+print_r($students);
+arsort($students);
+echo "\nSorted array in descending order:\n";
+print_r($students);
+?>
+
