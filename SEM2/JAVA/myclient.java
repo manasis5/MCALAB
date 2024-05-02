e70b7dd6eeb51bcf10b70f7adaae355138ca2d1c
manasis5 <150990515+manasis5@users.noreply.github.com>
2024-05-02 14:33:09 +0530

    Add files via upload

---
+++ SEM2/JAVA/myclient.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+import java.net.*;
+public class myclient {
+    public static void main(String[] args) {
+        try{
+            Socket socket=new Socket("localhost",7011);
+            DataOutputStream dout=new DataOutputStream(socket.getOutputStream());
+            dout.writeUTF("Client Call!!!");
+            dout.flush();
+            dout.close();
+            socket.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+}
